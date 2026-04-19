c03bb957b7db141c7890b8e6845252c56c9e9001
AlejandroClaure <alejandroclaure01@gmail.com>
2026-04-18 21:17:04 -0300

    agregué un Footer y lo añadí al layout
    añadí el footer completo, con links simulados y bien ordenado

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Header } from "../Header/Header";
+import { Footer } from "../Footer/Footer";
 
 function Layout() {
   const [search, setSearch] = useState("");
 
   return (
-    <>
+    <div className="min-h-screen flex flex-col bg-[#171a21]">
       <Header onSearch={setSearch} />
-      <Outlet context={{ search }} />
-    </>
+
+      <main className="flex-1">
+        <Outlet context={{ search }} />
+      </main>
+
+      <Footer />
+    </div>
   );
 }
 
