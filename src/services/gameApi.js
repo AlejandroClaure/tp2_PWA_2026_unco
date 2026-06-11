77a5db7d627607974fe22deb36512e726bb39626
AlejandroClaure <alejandroclaure01@gmail.com>
2026-06-11 14:01:23 -0300

    fix de una macana (mergee tarde y sobreescribi y se webio)
    arregle al gameApi como deberia quedar xd

--- src/services/gameApi.js
+++ src/services/gameApi.js
@@ -1,8 +1,36 @@
-console.log(import.meta.env.VITE_API_URL);
+/**
+ * =========================================================
+CONFIGURACIÓN DE API EN .env
+
+LOCAL:
+VITE_API_URL=http://localhost:3000/api
+
+PRODUCCIÓN (Vercel):
+VITE_API_URL=https:/laurldelautaro.vercel.app/api
+
+La URL se configura mediante variables de entorno. (si no está el archivo .env, crearlo en la raiz y agregar VITE_API_URL=http://localhost:3000/api)
+NO modificar BASE_URL manualmente.
+=========================================================
+ */
+
 const BASE_URL = `${import.meta.env.VITE_API_URL}/games`;
 
-export const getGames = async () => {
-  const res = await fetch(BASE_URL);
+export const getGames = async (
+  page = 1,
+  search = ""
+) => {
+  const params = new URLSearchParams({
+    page,
+    limit: 5,
+  });
+
+  if (search.trim()) {
+    params.append("search", search);
+  }
+
+  const res = await fetch(
+    `${BASE_URL}?${params.toString()}`
+  );
 
   if (!res.ok) {
     throw new Error("Error obteniendo juegos");
