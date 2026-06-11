8a0cf07dd306c6c29c8f49c33080f30c0047bd40
AlejandroClaure <alejandroclaure01@gmail.com>
2026-06-11 13:37:36 -0300

    Home y Search integrados al backend
    conecté el home y search al backend. Yuve que conectar el search al backend porque si no, solo buscaba en la lista de juegos cargados, mas no en la lista de juegos que el front no haya cargado. esto podría afectar cuando tenés una lista amplia en back y no cargó en dicha página. Ahora el search busca en el back directamente y trae las coincidencias! y home sigue con scroll infinito, dado a que puse un limite en el back

--- src/services/gameApi.js
+++ src/services/gameApi.js
@@ -1,13 +1,50 @@
-const BASE_URL = "https://69e2e9773327837a1552b35a.mockapi.io/api/v1/juegos";
+/**
+ * =========================================================
+CONFIGURACIÓN DE API EN .env
 
-export const getGames = async (page = 1, search = "") => {
+LOCAL:
+VITE_API_URL=http://localhost:3000/api
+
+PRODUCCIÓN (Vercel):
+VITE_API_URL=https:/laurldelautaro.vercel.app/api
+
+La URL se configura mediante variables de entorno. (si no está el archivo .env, crearlo en la raiz y agregar VITE_API_URL=http://localhost:3000/api)
+NO modificar BASE_URL manualmente.
+=========================================================
+ */
+
+const BASE_URL = `${import.meta.env.VITE_API_URL}/games`;
+
+export const getGames = async (
+  page = 1,
+  search = ""
+) => {
+  const params = new URLSearchParams({
+    page,
+    limit: 5,
+  });
+
+  if (search.trim()) {
+    params.append("search", search);
+  }
+
   const res = await fetch(
-    `${BASE_URL}?page=${page}&limit=5&search=${search}`
+    `${BASE_URL}?${params.toString()}`
   );
+
+  if (!res.ok) {
+    throw new Error("Error obteniendo juegos");
+  }
+
   return res.json();
 };
 
 export const getGameById = async (id) => {
   const res = await fetch(`${BASE_URL}/${id}`);
+
+  if (!res.ok) {
+    throw new Error("Juego no encontrado");
+  }
+
   return res.json();
 };
