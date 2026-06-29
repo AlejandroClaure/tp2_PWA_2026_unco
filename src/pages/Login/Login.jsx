72c2292adfb4a045eeb9b466a2ff18280a5dfe68
gastonllaupe <llaupeg@gmail.com>
2026-06-29 12:26:45 -0300

    cambio url

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -31,7 +31,7 @@ function Login() {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:3000/auth/login", {
+      const response = await fetch("http://localhost:3000/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
